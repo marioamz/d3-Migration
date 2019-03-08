9788c0e6cf9fccf44cffa6560c1cad6ba5688620
Mario Moreno <marioamz@gmail.com>
2019-03-08 07:52:28 -0600

    change in data

--- indexold.js
+++ indexold.js
@@ -1,114 +1,79 @@
-
-
-// I need to read in survey data with latin encodings and topojson data
-
-Promise.all([
-  d3.csv('data/encodedmex.csv'),
-  d3.csv('data/encodeusa.csv'),
-  d3.json('build/mx_tj.json')
-]).then(data => myVis(data));
-
-function myVis(data) {
-  const [depmex, depusa, muni] = data;
-  console.log(muni);
-
-  const width = 960;
-  const height = 600;
-  const margin = {
-    top: 10,
-    left: 10,
-    right: 10,
-    bottom: 10};
-
-  const svg = d3.select('body')
-  .append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .append('g')
-    .attr('transform', `translate(${margin.left},${margin.top})`);
-
-
-  const projection = d3.geoMercator();
-
-  const geoGenerator = d3.geoPath(projection);
-
-  const states = topojson.feature(muni, muni.objects.states),
-      municipalities = topojson.feature(muni, muni.objects.municipalities);
-
-  projection.fitSize([960, 600], states);
-
-  svg.selectAll('.state')
-    .data(municipalities)
-    .enter()
-    .append('path')
-      .attr('class', 'state')
-      .attr('stroke', 'black')
-      .attr('d', d => geoGenerator(d));
-
-};
-
-
-
-/*
-
-g.selectAll('path')
-  .data(states)
-  .enter()
-  .append('path')
-    .attr('class', 'state')
-    .attr('stroke', 'black')
-    .attr('d', geoGenerator);
-
-
-// finally we construct our rendered states
-  svg.selectAll('.state')
-    .data(stateShapes.features)
-    .enter()
-    .append('path')
-      .attr('class', 'state')
-      .attr('stroke', 'black')
-      .attr('fill', d => colorScale(stateNameToPop[d.properties.State]))
-      .attr('d', d => geoGenerator(d));
-
-
-
-
-var x = d3.scaleLinear()
-    .domain([0, width])
-    .range([0, width]);
-
-var y = d3.scaleLinear()
-    .domain([0, height])
-    .range([height, 0]);
+// this is to build the Mexico map
 
 var width = 960,
     height = 500;
 
 var projection = d3.geoMercator()
-    .scale(1200)
+    .scale(1100)
     .center([-102.34034978813841, 24.012062015793]);
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-var g = svg.append("g");
+//var g = svg.append("g");
 
 d3.json("build/mx_tj.json", function(error, mx) {
   svg.selectAll("path")
-    .data(topojson.object(mx, mx.objects.municipalities).geometries)
+    .data(topojson.object(mx, mx.objects.states).geometries)
     .enter().append("path")
     .attr("d", d3.geoPath().projection(projection))
     .attr("fill", "transparent")
     .style("stroke", "#333")
     .style("stroke-width", ".2px")
     .attr("class", "muns");
-
-  g.selectAll("path")
-    .data(topojson.object(mx, mx.objects.states).geometries)
-    .enter().append("path")
-    .attr("d", d3.geoPath().projection(projection))
-    .attr("fill", "transparent")
-    .style("stroke", "#333");
 });
-*/
+
+// now I'm building dots for people deported from USA
+
+// WORKS, ITERATING THROUGH COLUMNS
+var columns = [['mex_port_long', 'mex_port_lat'],
+['city_long', 'city_lat']]
+
+d3.csv('d3data/gooeydata.csv', function(error, usa) {
+  let state = 0;
+  const newData = usa.map(row => {
+    return {
+      ...row,
+      locations: columns.map(column => {
+        return [Number(row[column[0]]), Number(row[column[1]])];
+      })
+    };
+  });
+
+  console.log(newData)
+
+  svg.selectAll('circle')
+    .data(newData)
+    .enter()
+    .append('circle')
+    .attr('class', 'mexport')
+    .attr('cx', function(d) {
+      return projection([d.locations[state][0], d.locations[state][1]])[0];
+      // return projection([d[columns[i][0]], d[columns[i][1]]])[0];
+    })
+    .attr('cy', function(d) {
+      return projection([d.locations[state][0], d.locations[state][1]])[1];
+    })
+    .attr('r', 2);
+
+  d3.select("#option").select("input")
+    .on('click', d => {
+      state += 1;
+      console.log(state);
+
+      svg.selectAll('.mexport')
+        .transition().duration(1000)
+        .attr('cx', function(d) {
+          return projection([d.locations[state][0], d.locations[state][1]])[0];
+        // return projection([d[columns[i][0]], d[columns[i][1]]])[0];
+        })
+        .attr('cy', function(d) {
+          return projection([d.locations[state][0], d.locations[state][1]])[1];
+        });
+
+    })
+    .text('asddsadsa')
+  // for(var i=0; i < columns.length; i++) {
+  // }
+});
