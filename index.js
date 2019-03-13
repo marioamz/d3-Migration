7a1e58c5549656c9601e96834dac23d8ea3ed1ac
Mario Moreno <marioamz@gmail.com>
2019-03-13 12:53:59 -0500

    three working graphs

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//
+
 
 
 var width = 960,
@@ -7,64 +9,104 @@ var projection = d3.geoMercator()
     .scale(1100)
     .center([-102.34034978813841, 24.012062015793]);
 
-var svg2 = d3.select('#caravan_graph')
-      .append('svg')
-      .attr('width', width)
-      .attr('height', height);
+var path = d3.geoPath().projection(projection);
 
-var g1 = svg2.append('g');
+var svg3 = d3.select("#chloropeth").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
+var colorScheme = d3.schemePurples[4];
+        colorScheme.unshift("#eee");
+var colorScale = d3.scaleThreshold()
+            .domain([0, 0.12, 0.24, 0.36, 0.48, 0.6])
+            .range(colorScheme);
+        // Legend
+var g = svg3.append("g")
+        .attr("class", "legendThreshold")
+        .attr("transform", "translate(20,20)");
+    g.append("text")
+        .attr("class", "caption")
+        .attr("x", 0)
+        .attr("y", -6)
+        .text("% Experienced Danger");
+var labels = ['0-12%', '12-24%', '24-36%', '36-48%', '48-60%'];
+var legend = d3.legendColor()
+        .labels(function (d) { return labels[d.i]; })
+        .shapePadding(4)
+        .scale(colorScale);
+      svg3.select(".legendThreshold")
+        .call(legend);
+
+
+const url = 'https://gist.githubusercontent.com/ponentesincausa/46d1d9a94ca04a56f93d/raw/a05f4e2b42cf981e31ef9f6f9ee151a060a38c25/mexico.json';
+
+d3.csv("d3data/violence.csv", function(data) {
+  d3.json(url, function(error, mx) {
+    for (var i = 0; i < data.length; i++) {
+      var dataState = data[i].State;
+      var dataValue = parseFloat(data[i].Percentage);
+      for (var j = 0; j < mx.features.length; j++) {
+        var jsonState = mx.features[j].properties.name;
+          if (dataState == jsonState) {
+            mx.features[j].properties.value = dataValue;
+            console.log(dataState, jsonState, dataValue);
+
+            break;
+          }
+        }
+      }
+
+
+
+    svg3.selectAll("path")
+      .data(mx.features)
+      .enter().append("path")
+      .attr("d", path)
+      .classed("area",true)
+      .on('mouseover', function(d) {
+        d3.select(this).classed("highlight",true);
+          drawTooltip(d);})
+      .on('mouseout',mouseout)
+      .attr("fill", function(d) {
+        // d.total = d.properties.value;
+        console.log(d.properties.value);
+        //return '#fde0dd';
+        return colorScale(d.properties.value);
+        })
+      .style("stroke", "#333")
+      .style("stroke-width", ".2px")
+      .attr("class", "muns");
+  });
+});
+
+function drawTooltip(d){
+		var xPosition = d3.event.pageX;
+    var yPosition = d3.event.pageY;
+
+		d3.select("#tooltip")
+			.classed("hidden",false)
+			.style("left", xPosition + "px")
+			.style("top", yPosition + "px")
+			.text(d.properties.name, d.properties.value);
+}
+
+function mouseout() {
+	d3.select("#tooltip").classed("hidden", true);
+	d3.select(this).classed("highlight",false)
+}
+
+
+/* THIS IS THE CODE FOR CREATING MULTIPLE ITERATIONS OF THE SAME GRAPH
 d3.json("build/mx_tj.json", function(error, mx) {
-        g1.selectAll("path")
-          .data(topojson.object(mx, mx.objects.states).geometries)
-          .enter().append("path")
-          .attr("d", d3.geoPath().projection(projection))
-          .attr("fill", "transparent")
-          .style("stroke", "#333")
-          .style("stroke-width", ".2px")
-          .attr("class", "muns");
-        });
-
-var cols = [['city_long', 'city_lat']]
-
-d3.csv('d3data/Migrant_Caravan.csv', function(error, ups) {
-      let states = 0;
-        const newsData = ups.map(row => {
-          return {
-            ...row,
-          locations: cols.map(col => {
-            return [Number(row[col[0]]), Number(row[col[1]])];
-              })
-            };
-          });
-
-var line = d3.line()
-        .x(function(d) { return projection([d.locations[states][0], d.locations[states][1]])[0]; })
-        .y(function(d) { return projection([d.locations[states][0], d.locations[states][1]])[1]; })
-        .curve(d3.curveCardinal.tension(1));
-
-svg2.append("path")
-          .data([newsData])
-          .attr("class", "line")
-          .style("stroke", '#c51b8a')
-          .style("fill", "none")
-          .style("stroke-width", "3px")
-          .attr("d", line);
-
-transition(d3.selectAll('path'));
-
-function tweenDash() {
-       			var l = this.getTotalLength(),
-       				i = d3.interpolateString("0," + l, l + "," + l);
-       			return function (t) { return i(t); };
-       		};
-
-function transition(selection) {
-       		selection.each(function(){
-            d3.select(this).transition()
-       			.duration(15000)
-       			.attrTween("stroke-dasharray", tweenDash);
-             })
-       		};
-
+    const join = g1.selectAll("path")
+      .data(topojson.object(mx, mx.objects.states).geometries);
+    join
+      .enter().append("path")
+      .attr("d", path)
+      .merge(join)
+      .attr("fill", "transparent")
+      .style("stroke", "#333")
+      .style("stroke-width", ".2px")
+      .attr("class", "muns");
     });
+*/
--- indexold.js
+++ indexold.js
@@ -1,4 +1,5 @@
 // this is to build the Mexico map
+const url = 'https://gist.githubusercontent.com/ponentesincausa/46d1d9a94ca04a56f93d/raw/a05f4e2b42cf981e31ef9f6f9ee151a060a38c25/mexico.json';
 
 var width = 960,
     height = 500;
@@ -7,15 +8,17 @@ var projection = d3.geoMercator()
     .scale(1100)
     .center([-102.34034978813841, 24.012062015793]);
 
+var path = d3.geoPath().projection(projection);
+
 var svg = d3.select("#cities_graph").append("svg")
     .attr("width", width)
     .attr("height", height);
 
-d3.json("build/mx_tj.json", function(error, mx) {
+d3.json(url, function(error, mx) {
   svg.selectAll("path")
-    .data(topojson.object(mx, mx.objects.states).geometries)
+    .data(mx.features)
     .enter().append("path")
-    .attr("d", d3.geoPath().projection(projection))
+    .attr("d", path)
     .attr("fill", "transparent")
     .style("stroke", "#333")
     .style("stroke-width", ".2px")
@@ -89,11 +92,11 @@ var svg2 = d3.select('#caravan_graph')
 
 var g1 = svg2.append('g');
 
-d3.json("build/mx_tj.json", function(error, mx) {
+d3.json(url, function(error, mx) {
     g1.selectAll("path")
-      .data(topojson.object(mx, mx.objects.states).geometries)
+      .data(mx.features)
       .enter().append("path")
-      .attr("d", d3.geoPath().projection(projection))
+      .attr("d", path)
       .attr("fill", "transparent")
       .style("stroke", "#333")
       .style("stroke-width", ".2px")
@@ -147,60 +150,83 @@ d3.csv('d3data/Migrant_Caravan.csv', function(error, ups) {
 
 // THIS CREATES THE CHLOROPETH OF VIOLENCE
 
-var svg3 = d3.select('#chloropeth')
-    .append('svg')
-    .attr('width', width)
-    .attr('height', height);
+var svg3 = d3.select("#chloropeth").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
 var colorScheme = d3.schemePurples[4];
-    colorScheme.unshift("#eee")
+        colorScheme.unshift("#eee");
 var colorScale = d3.scaleThreshold()
-        .domain([0, 0.12, 0.24, 0.36, 0.48, 0.6])
-        .range(colorScheme);
-
-    // Legend
+            .domain([0, 0.12, 0.24, 0.36, 0.48, 0.6])
+            .range(colorScheme);
+        // Legend
 var g = svg3.append("g")
-    .attr("class", "legendThreshold")
-    .attr("transform", "translate(20,20)");
-g.append("text")
-    .attr("class", "caption")
-    .attr("x", 0)
-    .attr("y", -6)
-    .text("% Experienced Danger");
+        .attr("class", "legendThreshold")
+        .attr("transform", "translate(20,20)");
+    g.append("text")
+        .attr("class", "caption")
+        .attr("x", 0)
+        .attr("y", -6)
+        .text("% Experienced Danger");
 var labels = ['0-12%', '12-24%', '24-36%', '36-48%', '48-60%'];
 var legend = d3.legendColor()
-    .labels(function (d) { return labels[d.i]; })
-    .shapePadding(4)
-    .scale(colorScale);
-  svg3.select(".legendThreshold")
-    .call(legend);
+        .labels(function (d) { return labels[d.i]; })
+        .shapePadding(4)
+        .scale(colorScale);
+      svg3.select(".legendThreshold")
+        .call(legend);
 
 d3.csv("d3data/violence.csv", function(data) {
+  d3.json(url, function(error, mx) {
+    for (var i = 0; i < data.length; i++) {
+      var dataState = data[i].State;
+      var dataValue = parseFloat(data[i].Percentage);
+      for (var j = 0; j < mx.features.length; j++) {
+        var jsonState = mx.features[j].properties.name;
+          if (dataState == jsonState) {
+            mx.features[j].properties.value = dataValue;
+            console.log(dataState, jsonState, dataValue);
 
-  d3.json("build/mx_tj.json", function(json) {
-      for (var i = 0; i < data.length; i++) {
-        var dataState = data[i].Index;
-        var dataValue = parseFloat(data[i].Percentage);
-        for (var j = 0; j < json.objects.states.geometries.length; j++) {
-          var jsonState = parseFloat(json.objects.states.geometries[j].properties.CVE_ENT);
-            if (dataState == jsonState) {
-              json.objects.states.geometries[j].properties.value = dataValue;
-              break;
-            }
+            break;
           }
         }
-    console.log(json);
+      }
+
+
+
     svg3.selectAll("path")
-      .data(topojson.object(json, json.objects.states).geometries)
+      .data(mx.features)
       .enter().append("path")
-      .attr("d", d3.geoPath().projection(projection))
-      .style('fill', function(d) {
-        d.total = d.properties.value;
-        console.log(d.total);
-          return colorScale(d.total);
+      .attr("d", path)
+      .classed("area",true)
+      .on('mouseover', function(d) {
+        d3.select(this).classed("highlight",true);
+          drawTooltip(d);})
+      .on('mouseout',mouseout)
+      .attr("fill", function(d) {
+        // d.total = d.properties.value;
+        console.log(d.properties.value);
+        //return '#fde0dd';
+        return colorScale(d.properties.value);
         })
       .style("stroke", "#333")
       .style("stroke-width", ".2px")
       .attr("class", "muns");
-    });
+  });
 });
+
+function drawTooltip(d){
+		var xPosition = d3.event.pageX;
+    var yPosition = d3.event.pageY;
+
+		d3.select("#tooltip")
+			.classed("hidden",false)
+			.style("left", xPosition + "px")
+			.style("top", yPosition + "px")
+			.text(d.properties.name, d.properties.value);
+}
+
+function mouseout() {
+	d3.select("#tooltip").classed("hidden", true);
+	d3.select(this).classed("highlight",false)
+}
